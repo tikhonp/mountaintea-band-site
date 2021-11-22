cd249b6dbb08647c660eeb50e0a10badd190c08e
TikhonP <tikhonp@users.noreply.github.com>
2021-11-22 14:43:46 +0300

    add sentry

--- gornijchaij/settings/production.py
+++ gornijchaij/settings/production.py
@@ -1,4 +1,20 @@
 from .base import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://d0ff5c0a5a2941dabffa3896af63603e@o1075119.ingest.sentry.io/6075341",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 DEBUG = False
 
