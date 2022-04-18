265e54217e14a941d6987498f9313bed11501679
TikhonP <tikhon.petrishchev@gmail.com>
2022-04-18 17:48:20 +0300

    Fixed domain

--- concert/static/js/submit_issue.js
+++ concert/static/js/submit_issue.js
@@ -1,4 +1,4 @@
-const base_url = 'http://127.0.0.1:8000'
+const base_url = 'https://mountainteaband.ru'
 
 function validEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
