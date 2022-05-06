1f95332de84e00d17e40299c7a572baf6d913277
TikhonP <tikhon.petrishchev@gmail.com>
2022-05-06 23:41:22 +0300

    Added resend tickets

--- concertstaff/management/commands/resend_tickets_email.py
+++ concertstaff/management/commands/resend_tickets_email.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from concert.emails import generate_ticket_email, send_mail
 from concert.models import Transaction, Concert
 from django.utils import timezone
+from datetime import datetime
 
 
 class Command(BaseCommand):
@@ -14,7 +15,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         concert = Concert.objects.get(id=3)
         transactions = Transaction.objects.select_related('concert', 'user').filter(
-            is_done=True, concert=concert, date_closed__lte=timezone.now()
+            is_done=True, concert=concert, date_closed__lte=datetime(2022, 5, 6, 22, 31)
         )
         for transaction in transactions:
             print(transaction.id, transaction.user.first_name, transaction.user.email, concert.title)
