eeb2af0edd7fefb2232872de71187fb2b9576819
TikhonP <tikhon.petrishchev@gmail.com>
2025-02-06 23:46:10 +0300

    Fixed prices set list

--- concert/templatetags/my_filters.py
+++ concert/templatetags/my_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def is_active(things, state):
+    return things.filter(is_active=state)
