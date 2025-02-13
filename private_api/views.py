1cb8ea41cab431f63ce6b17f1adcfc0abb15b15d
TikhonP <tikhon.petrishchev@gmail.com>
2025-02-13 20:16:45 +0300

    CF caching POST requests??? So weird...

--- private_api/views.py
+++ private_api/views.py
@@ -134,18 +134,26 @@ class SmtpbzWebhookView(generics.GenericAPIView):
     serializer_class = SmtpbzEventPayloadSerializer
 
     def post(self, request, event):
+
+        # Seems like cloudflare was caching emails status
+        # requests. SO i tried to STOP IT!!!
+        resp = response.Response()
+        resp['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        resp['Pragma'] = 'no-cache'
+        resp['Expires'] = '0'
+
         tag = request.data.get('tag')
         if not tag:
-            return response.Response()
+            return resp
         tid = json.loads(tag).get('tid')
         if not tid:
-            return response.Response()
+            return resp
         transaction = get_object_or_404(Transaction, id=int(tid))
         message_status = request.data.get('message_status')
         transaction.email_status = event
         transaction.email_delivery_message = f"{message_status} {request.data.get('response')}"
         transaction.save()
-        return response.Response()
+        return resp
 
 
 class MailgunWebhookView(generics.GenericAPIView):
